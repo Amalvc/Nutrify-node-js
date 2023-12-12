0daf1feb70c5927fa25d56fa4198c6168cf19f06
AMAL V CHANDRAN <amalram301@gmail.com>
2023-12-12 22:02:49 +0530

    Updated

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 
 const userModel = require('./Models/userModel');
 
@@ -32,6 +34,30 @@ app.post('/register', async (req,res)=>{
     catch(error){
         console.error(error);
         res.status(500).send('Internal Server Error');
+    }
+})
+
+app.post('/login', async (req,res)=>{
+    let{email,password}=req.body;
+    try{
+        const user=await userModel.findOne({email});
+        if(!user){
+            res.status(404).send({message:'User not found'});
+        }
+        const isValid=bcrypt.compare(password,user.password);
+        if(isValid){
+            console.log(process.env.JWT_SECRET);
+            const token=jwt.sign({email:email},process.env.JWT_SECRET,{expiresIn:'1h'});
+            res.json({token});
+        }
+        else{
+            res.status(401).send({message:"Wrong credential"});
+        }  
+
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 })
 
